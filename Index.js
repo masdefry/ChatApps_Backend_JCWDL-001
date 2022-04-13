ff24113eaf483ce2c585b150b184d6942fbd40ac
masdefry <muhammaddefryan@gmail.com>
2022-04-13 11:45:07 +0700

    Total User

--- Index.js
+++ Index.js
@@ -16,9 +16,32 @@ app.get('/', (req, res) => {
     res.send('Welcome to API Chat')
 })
 
+// Socket.io -> 2 logic -> On, Emit
+let userConnected = []
 io.on('connection' , (socket) => {
     console.log('User Connect')
     console.log(socket.id)
+
+    socket.on('send-data', (name) => {
+        socket.broadcast.emit('broadcast', name)
+    })
+
+    socket.on('user-join', ({username, room}) => {
+        userConnected.push({
+            id: socket.id,
+            username: username, 
+            room: room
+        })
+
+        console.log(userConnected)
+
+        let checkTotalUserInRoom = userConnected.filter((value) => {
+            return value.room === room
+        })
+
+        socket.join(room)
+        socket.emit('total-user', checkTotalUserInRoom.length)
+    })
 
     socket.on('disconnect', () => {
         console.log('User Disconnect')
